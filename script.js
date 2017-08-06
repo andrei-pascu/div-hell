e42312ddc363ed0c434326e2a9b1065e647afdfa
Andrei Cristian Pascu <pascuandreicristian@yahoo.com>
2017-08-06 20:06:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// soon
+anime({
+  targets: '.w',
+  translateX: function() { return anime.random(-6, 6) + 'rem'; },
+  translateY: function() { return anime.random(-6, 6) + 'rem'; },
+  scale: function() { return anime.random(10, 20) / 10; },
+  rotate: function() { return anime.random(-360, 360); },
+  delay: function() { return 400 + anime.random(0, 500); },
+  duration: function() { return anime.random(1000, 2000); },
+  direction: 'alternate',
+  loop: true
+});
